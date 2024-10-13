4482d303336caa9d8f195e0d2200e13d6bfec8db
andriashengeli <andriashengelia5@gmail.com>
2024-10-13 18:51:34 +0400

    second commit

---
+++ level 44/classwork/44.py
@@ -0,0 +1,11 @@
+def max_number(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+
+print(max_number(10,200))
+print(max_number(5,400))
+print(max_number(10,100))
+
