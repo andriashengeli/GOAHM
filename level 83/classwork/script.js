fe0ecd93c37cd349a8d22f7f3c93223cbe0cd0e2
andriashengeli <andriashengelia5@gmail.com>
2024-12-29 12:47:47 +0400

    commit

---
+++ level 83/classwork/script.js
@@ -0,0 +1,11 @@
+let age = Number(prompt("Enter your age: "));
+
+if (age <= 5) {
+    console.log("You are a baby");
+} else if (age <= 12) {
+    console.log("You are a teenager");
+} else if (age <= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are old");
+}
---
+++ level 83/classwork/script2.js
@@ -0,0 +1,21 @@
+let num1 =Number(prompt("enter numbers (num1)"))
+let num2 =Number(prompt("enter numbers (num2)"))
+
+
+let math = num1 * num2
+
+alert(math)
+
+let Math = num1 / num2 
+
+alert(Math)
+
+
+let MATH = num1 - num2 
+
+alert(MATH)
+
+
+let NUMBER = num1 + num2
+
+alert(NUMBER)
