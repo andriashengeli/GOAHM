fd123056afac4485c19dab5f7d0de73379e7788d
andriashengeli <andriashengelia5@gmail.com>
2025-01-03 13:23:12 +0400

    commit

---
+++ level 83/homework/script.js
@@ -0,0 +1,9 @@
+let number = 10;
+
+let num = Number(prompt("Enter any number you want: "));
+
+if (num % 2 === 0) {
+  console.log("The number is even");
+} else {
+  console.log("The number is odd");
+}
