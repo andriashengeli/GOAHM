8bf3a69649118a8a1e8cc5b7a242c1b4b940f22e
andriashengeli <andriashengelia5@gmail.com>
2025-01-08 13:05:23 +0400

    commti

---
+++ level 88/classwork/script4.js
@@ -0,0 +1,6 @@
+let number = 1000
+
+while (number >0){
+    console.log(number)
+    number -= 5
+}
---
+++ level 88/classwork/script5.js
@@ -0,0 +1,5 @@
+let num=Number(prompt("enter number you want: "))
+while(num>=0){
+    console.log(num)
+    num--;
+}
