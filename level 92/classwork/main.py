793cb9a4e9cf61bbdf6f08cd4f177495db8b2f77
andriashengeli <andriashengelia5@gmail.com>
2025-01-19 20:48:49 +0400

    commit

---
+++ level 92/classwork/main.py
@@ -0,0 +1,18 @@
+#   1  mkdir homework
+#   2  mkdit "level 92"
+#   3  mkdir "level 92"
+#   4  cd level 92
+#   5  cd level 92
+#   6  cd "level 92"
+#   7  touch "classwork"
+#   8  mkdir "classwork"
+#   9  mkdir "homework"
+#   10  cd "classwor"
+#   11  cd "classwork"
+#   12  touch "main.py"
+#   13  ls
+#   14  pwd
+#   15  history
+#   16 -r "classwork"
+#   17 bash "homework"
+
