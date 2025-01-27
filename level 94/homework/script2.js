3f7f72efea0e8ac43273cc8afe493a60068b840c
andriashengeli <andriashengelia5@gmail.com>
2025-01-27 22:29:08 +0400

    commit:

---
+++ level 94/homework/script2.js
@@ -0,0 +1,11 @@
+let num =Number(prompt("enter you age: "));
+
+let res=(num > 18) ? "თქვენ წარმატებული ხართ" : "გჭირდებათ მეტი სწავლა" 
+
+
+let goa=(num < 18) ? "ჯერ ბავშვი ხარ მაგრამ შეგიძლია მიახწიო წარმატებას" : "წარმატებები"
+
+
+console.log(res)
+
+console.log(goa)
