1e1b10d1138e01e950730568478ec82d6d1d7a63
andriashengeli <andriashengelia5@gmail.com>
2025-02-17 23:15:38 +0400

    commit

---
+++ level 104/classwork/script.js
@@ -0,0 +1,9 @@
+let fruits = ["grapes","apple","orange","blueberrys","banana"];
+let num1 = fruits.splice(3,4, "ლეღვი")
+console.log(fruits)
+console.log(num1)
+
+
+let num2 = fruits.join("--")
+console.log(fruits)
+console.log(num2)
