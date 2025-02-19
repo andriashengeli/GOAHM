d32c262c09475b838f56a3c249cc21afee252df5
andriashengeli <andriashengelia5@gmail.com>
2025-02-19 15:50:31 +0400

    commit

---
+++ level 105/homework/script2.js
@@ -0,0 +1,40 @@
+let num1 = [10,20,30,40,50]
+
+
+let num2 = [60,70,80]
+
+let result = num1.concat(num2)
+
+console.log(result)
+
+
+let num3 = num1.fill(100,0,3)
+
+console.log(num3)
+
+
+let num4 = num1.pop()
+
+console.log(num4)
+
+let num5 = num1.shift()
+
+console.log(num5)
+
+
+let num6 = num1.unshift(5,15)
+
+console.log(num6)
+
+
+let num7 = num1.join("--")
+
+console.log(num7)
+
+num8 = num1.includes(30)
+
+console.log(num8)
+
+num9 = num1.lastIndexOf(30)
+
+console.log(num9)
