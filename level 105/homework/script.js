92e69c5198b2102e61358c6afe79a97059142505
andriashengeli <andriashengelia5@gmail.com>
2025-02-19 15:38:35 +0400

    commit

---
+++ level 105/homework/script.js
@@ -0,0 +1,58 @@
+let arr1 = [1, 2, 3, 4];
+
+let arr2 = [5, 6, 7, 8];
+
+let combinedArray = arr1.concat(arr2);
+
+
+console.log(combinedArray)
+
+
+arr1.copyWithin(0,3,5)
+
+console.log(arr1)
+
+let removed= arr1.pop();
+
+console.log(arr1)
+
+
+let removed2 = arr2.pop();
+
+console.log(removed2)
+
+
+let removed3 = arr1.shift();
+
+console.log(removed3)
+
+let removed7 = arr2.shift();
+
+console.log(removed3)
+
+
+let removed4 = arr1.unshift();
+
+console.log(removed4)
+
+
+let nothing = arr1.flat(1)
+
+console.log(nothing)
+
+
+let nothing1 = arr2.flat(1)
+
+console.log(nothing)
+
+
+let nothing2 = arr1.includes(8)
+
+
+console.log(nothing2)
+
+
+let nothing9 = arr2.includes(8)
+
+
+console.log(nothing2)
