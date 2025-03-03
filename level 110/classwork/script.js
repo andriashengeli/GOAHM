87ac10c54f7d49111959848866287ab37cd9ba11
andriashengeli <andriashengelia5@gmail.com>
2025-03-03 15:42:55 +0400

    commit

---
+++ level 110/classwork/script.js
@@ -0,0 +1,21 @@
+let button = document.getElementById("goa");
+button.style.color="red";
+button.style.fontFamily="cursive";
+button.style.fontSize="10px";
+button.innerHTML="<button>im chad</button>";
+
+
+
+let p = document.getElementsByClassName("aboutme");
+p[0].style.color="orange";
+p[0].style.fontFamily="cursive";
+p[0].style.fontSize="20px";
+p.innerText="andria shengelia"
+
+
+
+let num = document.getElementsByTagName("p");
+num[1].style.color="blue";
+num[1].style.fontFamily="cursive";
+num[1].style.fontSize="30px";
+num.innerHTML="<p>idk</p>";
---
+++ level 110/homework/script.js
@@ -0,0 +1,19 @@
+let p = document.getElementById("num1");
+p.style.color = "red";
+p.style.backgroundColor = "blue";
+p.style.fontFamily = "cursive";;
+p.innerText="im chad"
+
+
+let p2 = document.getElementsByClassName("idk");
+p2[0].style.color = "green";
+p2[0].style.backgroundColor = "orange";
+p2[0].style.fontFamily = "cursive";
+
+
+
+
+let idk = document.getElementsByTagName("p");
+idk[1].style.color = "green";
+idk[1].style.backgroundColor = "aqua";
+idk[1].style.fontFamily = "cursive";
