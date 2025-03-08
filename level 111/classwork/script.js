cdd25ccc58ffdb68937ecca3d556c5f73a814f73
andriashengeli <andriashengelia5@gmail.com>
2025-03-08 12:01:41 +0400

    commit

---
+++ level 111/classwork/script.js
@@ -0,0 +1,14 @@
+function clickToApple() {
+    
+    document.getElementById('fruitImage').src = "apple.png";
+}
+
+function clickToBanana() {
+    
+    document.getElementById('fruitImage').src = "banana.png";
+}
+
+function clickToCherry() {
+    
+    document.getElementById('fruitImage').src = "cherry.png";
+}
---
+++ level 111/homework/script.js
@@ -0,0 +1,6 @@
+let text = document.createElement("P");
+text.textContent = "Hello"
+
+document.body.appendChild(text);
+
+console.log(text);
