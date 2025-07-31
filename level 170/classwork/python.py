69500bec855f5f2dda292c0c2eaaf35366aed0cd
andriashengeli <andriashengelia5@gmail.com>
2025-07-31 14:01:43 +0400

    commit

---
+++ level 170/classwork/python.py
@@ -0,0 +1,6 @@
+number = input("Enter a age: ")
+
+if number < 18:
+    print("you can go to airplane")
+elif number >= 18:
+    print("take your kindle!")
---
+++ level 170/classwork/python2.py
@@ -0,0 +1,10 @@
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+word1 = int(input())
+word2 = int(input())
+word3 = int(input())
+
+combined = alphabet[word1] + alphabet[word2] + alphabet[word3]
+
+print(combined)
