c79e2e9eaa522a049914aead83c2aea61049fd96
andriashengeli <andriashengelia5@gmail.com>
2025-07-31 20:13:35 +0400

    commit

---
+++ level 170/homework/python.py
@@ -0,0 +1,14 @@
+name = "andria"
+# this is gonna be a string because it is in qoutes 
+
+age = 14
+# this is an integer because it is a number and it doesent have any qoutes
+
+
+height = 1.50 
+# this is a float because its a number but have an dot and becasue of thats it is a float
+
+is_student = True
+# this is a boolean because it is either true or false
+
+
---
+++ level 170/homework/python2.py
@@ -0,0 +1,9 @@
+name = int(input("enter your age: "))
+
+if name < 18:
+    print("you are an adult and you can login now ")
+
+else:
+    print("you are not an adult and you cannot login now ")
+
+
